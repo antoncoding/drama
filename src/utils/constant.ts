095d890482c1761a7d5fe225264ce877f8d23e19
Anton Cheng <antonassocareer@gmail.com>
2022-03-02 15:53:05 +0100

    misc: add Ukrane address

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -5,9 +5,12 @@ export const spammers = [
 
 export const featuring: { account: string; tag?: string }[] = [
   {
+    account: "0x165cd37b4c644c2921454429e7f9358d18a45e14",
+    tag: "Donate to Ukraine 🇺🇦",
+  },
+  {
     account: "0x981c8ecd009e3e84ee1ff99266bf1461a12e5c68",
     tag: "JuiceBox (AssangeDAO)",
   },
   { account: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" },
-  { account: "0x099f8498348792abe502ec12f6163254dbbe321e" },
 ];
